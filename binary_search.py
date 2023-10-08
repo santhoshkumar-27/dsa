6b47c50acf1d2f5747355c3e23f67fb869926872
santhoshkumar <its.santhoshkumar@outlook.com>
2023-10-08 20:45:44 +0530

    done binary search

--- binary_search.py
+++ binary_search.py
@@ -1,20 +1,20 @@
 import linear_search
-    
-def binary_search(list, target):
-    first = 0
-    last = len(list) - 1
 
-    while first <= last:
-        midPoint = (first + last) // 2
+step = 0
+def binary_search(list, target, start, last):
+    if start > last:
+        return None
+    global step
+    step += 1
+    midPoint = (start + last) // 2
+    currentNumber = list[midPoint]
+    if target  ==  currentNumber:
+        return midPoint
+    elif target < currentNumber:
+        return binary_search(list, target, start, midPoint - 1)
+    elif target > currentNumber:
+        return binary_search(list, target, midPoint + 1, last)
 
-        if list[midPoint] == target:
-            return midPoint
-        elif list[midPoint] < target:
-            first = midPoint + 1
-        elif list[midPoint] > target:
-            last = midPoint - 1
-
-    return None
-
-lists = range(1, 11)
-linear_search.verify(binary_search(lists, 25))
+lists = range(1, 50000000000000)
+linear_search.verify(binary_search(lists, 11, 0, len(lists) - 1))
+print(step)
