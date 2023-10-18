befa3a362b1d771c4c46db5188fe44fc4b536082
santhoshkumar <its.santhoshkumar@outlook.com>
2023-10-18 07:14:04 +0530

    feat: added binary search and linear search

--- load.py
+++ load.py
@@ -7,3 +7,11 @@ def load_numbers(file_name):
         for line in f:
             numbers.append(int(line))
         return numbers
+
+
+def load_strings(file_name):
+  strings = []
+  with open(file_name) as f:
+    for line in f:
+      strings.append(line.rstrip())
+  return strings
